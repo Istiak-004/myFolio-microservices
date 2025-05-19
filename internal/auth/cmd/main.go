76f5b9c688d05fb2a9479338d66cbcdd0edf099f
Istiak-004 <istiak.dev.004@gmail.com>
2025-05-20 00:11:52 +0600

    added the config

--- internal/auth/cmd/main.go
+++ internal/auth/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	authconfig "github.com/istiak-004/myFolio-microservices/auth/config"
 	"github.com/istiak-004/myFolio-microservices/auth/internal/domain/models"
 	"github.com/istiak-004/myFolio-microservices/pkg/config"
 	"github.com/istiak-004/myFolio-microservices/pkg/logger"
@@ -11,12 +12,19 @@ import (
 func main() {
 	log := logger.NewLogger("Auth")
 	log.Info("Starting Auth Service", log.String("service", "auth"))
-	config, err := config.Init("Auth Service")
+
+	// Load configuration
+	log.Info("Loading configuration")
+	// Load the configuration using the config package
+	var authConfig authconfig.AuthConfig
+
+	base, config, err := config.LoadConfig("auth", "../config", &authConfig)
 	if err != nil {
 		log.WithError(err).Error("Failed to initialize configuration")
 		return
 	}
 	log.WithFields(map[string]interface{}{
+		"base":   base,
 		"config": config,
 	}).Info("Configuration loaded successfully")
 
